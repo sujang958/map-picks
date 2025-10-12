2a30553eed19b2f26b80089bd5bf9539c0988bba
sujang958 <sujangdev@gmail.com>
2025-10-12 23:56:42 +0900

    first

---
+++ apps/front/src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { onDestroy, onMount } from 'svelte';
+
+	onMount(() => {
+		const ws = new WebSocket('ws://localhost:3000/ws');
+
+		ws.onopen = () => {
+			console.log('WebSocket connection opened');
+			ws.send('Hello Server!');
+		};
+	});
+</script>
+
+hi
