26b6f93640d07caa66e3c7160be07391893e1490
sujang958 <enter7377@naver.com>
2025-10-13 14:55:20 +0900

    feat: add map pick compoennt

---
+++ apps/front/src/lib/components/MapPick.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	let { type = 'Veto', team = 'T1', enemyTeam = null, enemyPicks = 'DEFENSE' } = $props();
+</script>
+
+<div class="relative flex h-44 flex-col gap-y-2 bg-[url(/split.avif)] bg-contain text-white">
+	<p class="z-50 bg-black p-1.5 text-center text-sm font-medium text-white">{team} {type}</p>
+	<!-- <div><img src="/split.avif" alt="" /></div> -->
+	{#if type.toUpperCase() == 'VETO'}
+		<div class="absolute inset-0 z-10 bg-black/50">&nbsp;</div>
+	{/if}
+
+	{#if enemyTeam}
+		<p class="z-50 mt-auto w-full bg-black p-3 text-center text-xs text-white">
+			{enemyTeam} PICKS {enemyPicks}
+		</p>
+	{/if}
+</div>
--- apps/front/src/routes/+page.svelte
+++ apps/front/src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import MapPick from '$lib/components/MapPick.svelte';
 	import { onDestroy, onMount } from 'svelte';
 
 	onMount(() => {
@@ -11,4 +12,18 @@
 	});
 </script>
 
-hi
+<div class="grid h-screen w-full place-items-center">
+	<main class="w-full max-w-4xl rounded-lg p-3">
+		<h1 class="text-3xl font-semibold">Bo3 Map Picks</h1>
+		<h1 class="mt-2 text-base text-neutral-500">Time Limit 10:00</h1>
+		<div class="mt-8 grid grid-cols-7">
+			<MapPick team="T1" />
+			<MapPick team="T2" />
+			<MapPick team="T1" type="Select" enemyTeam="T2" />
+			<MapPick team="T2" type="Select" enemyTeam="T1" />
+			<MapPick team="T1" />
+			<MapPick team="T2" />
+			<MapPick team="" type="Remainder" enemyTeam="T1" />
+		</div>
+	</main>
+</div>
