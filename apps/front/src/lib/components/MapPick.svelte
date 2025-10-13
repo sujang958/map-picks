4d850da93f0d4f392f00c403d4492f04cf5bcbc8
sujang958 <sujangdev@gmail.com>
2025-10-13 20:23:00 +0900

    feat: add the header of the picking area

--- apps/front/src/lib/components/MapPick.svelte
+++ apps/front/src/lib/components/MapPick.svelte
@@ -1,17 +1,32 @@
 <script lang="ts">
-	let { type = 'Veto', team = 'T1', enemyTeam = null, enemyPicks = 'DEFENSE', map = "" } = $props();
+	import { twMerge } from 'tailwind-merge';
+
+	let {
+		type = 'Veto',
+		team = 'T1',
+		enemyTeam = null,
+		enemyPicks = 'DEFENSE',
+		map = '',
+		active = false
+	} = $props();
 </script>
 
-<div class="relative flex h-44 flex-col gap-y-2 bg-[url(/split.avif)] bg-contain text-white">
-	<p class="z-50 bg-black p-1.5 text-center text-sm font-medium text-white">{team} {type}</p>
+<div
+	class={twMerge('relative flex h-44 flex-col gap-y-2 bg-[url(/split.avif)] bg-contain text-white')}
+>
+	<p class="z-40 bg-black p-1.5 text-center text-xs font-medium text-white">{team} {type}</p>
 	<!-- <div><img src="/split.avif" alt="" /></div> -->
 	{#if type.toUpperCase() == 'VETO'}
 		<div class="absolute inset-0 z-10 bg-black/50">&nbsp;</div>
 	{/if}
 
 	{#if enemyTeam}
-		<p class="z-50 mt-auto w-full bg-black p-3 text-center text-xs text-white">
-			{enemyTeam} PICKS {enemyPicks}
+		<p class="z-40 mt-auto w-full bg-black p-3 text-center text-xs font-medium text-white">
+			{enemyTeam} PICKS <br />{enemyPicks}
 		</p>
+	{/if}
+
+	{#if active}
+		<div class="pointer-events-none absolute -inset-0.5 z-50 border-4 border-blue-400 rounded-lg"></div>
 	{/if}
 </div>
--- apps/front/src/routes/+page.svelte
+++ apps/front/src/routes/+page.svelte
@@ -13,17 +13,54 @@
 </script>
 
 <div class="grid h-screen w-full place-items-center">
-	<main class="w-full max-w-4xl rounded-lg p-3">
+	<main class="h-full w-full max-w-4xl rounded-lg p-3 py-24">
 		<h1 class="text-3xl font-semibold">Bo3 Map Picks</h1>
 		<h1 class="mt-2 text-base text-neutral-500">Time Limit 10:00</h1>
 		<div class="mt-8 grid grid-cols-7">
-			<MapPick team="T1" />
+			<MapPick team="T1" active />
 			<MapPick team="T2" />
 			<MapPick team="T1" type="Select" enemyTeam="T2" />
 			<MapPick team="T2" type="Select" enemyTeam="T1" />
 			<MapPick team="T1" />
 			<MapPick team="T2" />
 			<MapPick team="" type="Remainder" enemyTeam="T1" />
+		</div>
+
+		<div class="mt-16 flex flex-row justify-between gap-x-3">
+			<div>
+				<header class="text-left">
+					<p class="flex flex-row items-center gap-x-2 text-xl font-semibold">
+						T1 <svg
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 24 24"
+							fill="currentColor"
+							class="size-3 fill-red-800"
+						>
+							<path
+								d="M9.195 18.44c1.25.714 2.805-.189 2.805-1.629v-2.34l6.945 3.968c1.25.715 2.805-.188 2.805-1.628V8.69c0-1.44-1.555-2.343-2.805-1.628L12 11.029v-2.34c0-1.44-1.555-2.343-2.805-1.628l-7.108 4.061c-1.26.72-1.26 2.536 0 3.256l7.108 4.061Z"
+							/>
+						</svg>
+					</p>
+					<p class="mt-1 text-sm text-neutral-500">Choosing a map...</p>
+				</header>
+			</div>
+			<div>
+				<header class="text-right">
+					<p class="flex flex-row items-center gap-x-2 text-xl font-semibold">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 24 24"
+							fill="currentColor"
+							class="size-3 fill-neutral-800"
+						>
+							<path
+								d="M5.055 7.06C3.805 6.347 2.25 7.25 2.25 8.69v8.122c0 1.44 1.555 2.343 2.805 1.628L12 14.471v2.34c0 1.44 1.555 2.343 2.805 1.628l7.108-4.061c1.26-.72 1.26-2.536 0-3.256l-7.108-4.061C13.555 6.346 12 7.249 12 8.689v2.34L5.055 7.061Z"
+							/>
+						</svg> T2(You)
+					</p>
+					<p class="mt-1 text-sm text-neutral-500">Waiting...</p>
+				</header>
+			</div>
 		</div>
 	</main>
 </div>
